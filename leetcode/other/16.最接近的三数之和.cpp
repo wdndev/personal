34ac8344026a8615028a944bfc236454f50a9063
wdndev <dongnian.wang@outlook.com>
2023-10-19 17:36:52 +0800

    76

---
+++ leetcode/other/16.最接近的三数之和.cpp
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=16 lang=cpp
+ *
+ * [16] 最接近的三数之和
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+
+        return 0;
+    }
+};
+// @lc code=end
+
