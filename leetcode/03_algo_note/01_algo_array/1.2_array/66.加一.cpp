60ec259d0ad038307f736546052e6f5406aeb477
wdndev <dongnian.wang@outlook.com>
2023-12-12 11:24:39 +0800

    array

---
+++ leetcode/03_algo_note/01_algo_array/1.2_array/66.加一.cpp
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode.cn id=66 lang=cpp
+ *
+ * [66] 加一
+ *
+ * https://leetcode.cn/problems/plus-one/description/
+ *
+ * algorithms
+ * Easy (45.30%)
+ * Likes:    1334
+ * Dislikes: 0
+ * Total Accepted:    690.2K
+ * Total Submissions: 1.5M
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
+ * 
+ * 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+ * 
+ * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：digits = [1,2,3]
+ * 输出：[1,2,4]
+ * 解释：输入数组表示数字 123。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：digits = [4,3,2,1]
+ * 输出：[4,3,2,2]
+ * 解释：输入数组表示数字 4321。
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：digits = [0]
+ * 输出：[1]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * 0 
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        for (int i = n -1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                for (int j = i + 1; j < n; j++) {
+                    digits[j] = 0;
+                }
+                return digits;
+            }
+        }
+
+        // 所有元素均为9
+        std::vector<int> ans(n + 1);
+        ans[0] = 1;
+        return ans;
+    }
+};
+// @lc code=end
+
