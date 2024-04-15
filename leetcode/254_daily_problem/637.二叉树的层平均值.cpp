aa5df755f71c953b86ba54cfde1939251f36932b
wdndev <dongnian.wang@outlook.com>
2024-04-15 23:20:48 +0800

    0415

---
+++ leetcode/254_daily_problem/637.二叉树的层平均值.cpp
@@ -0,0 +1,98 @@
+/*
+ * @lc app=leetcode.cn id=637 lang=cpp
+ *
+ * [637] 二叉树的层平均值
+ *
+ * https://leetcode.cn/problems/average-of-levels-in-binary-tree/description/
+ *
+ * algorithms
+ * Easy (70.36%)
+ * Likes:    484
+ * Dislikes: 0
+ * Total Accepted:    190.3K
+ * Total Submissions: 270.1K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个非空二叉树的根节点 root , 以数组的形式返回每一层节点的平均值。与实际答案相差 10^-5 以内的答案可以被接受。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 
+ * 
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：[3.00000,14.50000,11.00000]
+ * 解释：第 0 层的平均值为 3,第 1 层的平均值为 14.5,第 2 层的平均值为 11 。
+ * 因此返回 [3, 14.5, 11] 。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 
+ * 
+ * 输入：root = [3,9,20,15,7]
+ * 输出：[3.00000,14.50000,11.00000]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 
+ * 
+ * 树中节点数量在 [1, 10^4] 范围内
+ * -2^31 <= Node.val <= 2^31 - 1
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        std::vector<double> ans;
+
+        std::queue<TreeNode*> queue;
+        queue.push(root);
+        TreeNode* tmp_node = nullptr;
+
+        while (!queue.empty()) {
+            double sum = 0;
+            int node_count = queue.size();
+            for (int i = 0; i < node_count; i++) {
+                tmp_node = queue.front();
+                queue.pop();
+                sum += tmp_node->val;
+
+                if (tmp_node->left) {
+                    queue.push(tmp_node->left);
+                }
+                if (tmp_node->right) {
+                    queue.push(tmp_node->right);
+                }
+            }
+            ans.push_back(double(sum / node_count));
+            sum = 0;
+        }
+
+
+        return ans;
+    }
+};
+// @lc code=end
+
