fb185eb0977ef6849b91b3e22ca2cf7f3949e780
wdndev <dongnian.wang@outlook.com>
2024-05-20 22:29:00 +0800

    0520

---
+++ leetcode/254_daily_problem/110.平衡二叉树.cpp
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode.cn id=110 lang=cpp
+ *
+ * [110] 平衡二叉树
+ *
+ * https://leetcode.cn/problems/balanced-binary-tree/description/
+ *
+ * algorithms
+ * Easy (58.37%)
+ * Likes:    1509
+ * Dislikes: 0
+ * Total Accepted:    613K
+ * Total Submissions: 1M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，判断它是否是 平衡二叉树  
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：true
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = [1,2,2,3,3,null,null,4,4]
+ * 输出：false
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = []
+ * 输出：true
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中的节点数在范围 [0, 5000] 内
+ * -10^4 <= Node.val <= 10^4
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        return this->dfs(root) != -1;
+    }
+
+    int dfs(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        int left_depth = this->dfs(root->left);
+        int right_depth = this->dfs(root->right);
+
+        if (left_depth == -1 || right_depth == -1) {
+            return -1;
+        }
+
+        return abs(left_depth - right_depth) < 2 ? max(left_depth, right_depth) + 1 : -1;
+    }
+};
+// @lc code=end
+
